ecb5a8bd377ba9a41753d817ad5aa97e93d11ce5
Bryan Koroleski <korol1bp@cmich.edu>
2010-10-03 22:55:32 -0400

    Implemented the first part of chapter 3.

--- Source/lottery.m
+++ Source/lottery.m
@@ -11,8 +11,22 @@
 int main (int argc, const char * argv[]) {
 
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    // insert code here...
-    NSLog(@"Hello, World!");
+    
+    NSMutableArray *array;
+    int i;
+    
+    array = [[NSMutableArray alloc] init];
+    
+    for(i=0; i<10; ++i) {
+        NSNumber *newNumber = [[NSNumber alloc] initWithInt:(i*3)];
+        [array addObject:newNumber];
+    }
+    
+    for(i=0; i<10; ++i) {
+        NSNumber *numberToPrint = [array objectAtIndex:i];
+        NSLog(@"The number at %d is %@", i, numberToPrint);
+    }
+    
     [pool drain];
     return 0;
 }
