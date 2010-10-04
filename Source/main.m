ff17169de9426394936254d84d9d2debfb070097
Bryan Koroleski <korol1bp@cmich.edu>
2010-10-03 22:42:21 -0400

    Committing project skeleton.

---
+++ Source/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  lottery
+//
+//  Created by Bryan Koroleski on 10/3/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    // insert code here...
+    NSLog(@"Hello, World!");
+    [pool drain];
+    return 0;
+}
+
