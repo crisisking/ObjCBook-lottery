b208f34dee3b1f7696bb0f4967502b210b069d05
Bryan Koroleski <korol1bp@cmich.edu>
2010-10-03 23:50:11 -0400

    Added skeleton for LotteryEntry class.

---
+++ Source/LotteryEntry.h
@@ -0,0 +1,17 @@
+//
+//  LotteryEntry.h
+//  lottery
+//
+//  Created by Bryan Koroleski on 10/3/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LotteryEntry : NSObject {
+@private
+    
+}
+
+@end
---
+++ Source/LotteryEntry.m
@@ -0,0 +1,28 @@
+//
+//  LotteryEntry.m
+//  lottery
+//
+//  Created by Bryan Koroleski on 10/3/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "LotteryEntry.h"
+
+
+@implementation LotteryEntry
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
